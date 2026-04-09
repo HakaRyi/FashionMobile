b36c30125beebb98a4bcb87f87651c3e52e2ca8d
하까리 <hxkhang1105@gmail.com>
2026-04-09 17:06:52 +0700

    add token to try on

--- lib/services/try_on_service.dart
+++ lib/services/try_on_service.dart
@@ -2,6 +2,7 @@
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:shared_preferences/shared_preferences.dart';
 import '../constants/api_constants.dart';
 
 class TryOnService {
@@ -14,7 +15,16 @@ class TryOnService {
     final url = Uri.parse("${ApiConstants.baseUrl}${ApiConstants.tryOnEndpoint}");
 
     try {
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString('token') ?? '';
+
       final request = http.MultipartRequest('POST', url);
+
+      request.headers.addAll({
+        'Authorization': 'Bearer $token',
+        'ngrok-skip-browser-warning': '69420',
+      });
+
       if (category != null) {
         request.fields['category'] = category.toString();
       }
